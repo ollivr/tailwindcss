01ded2df025d84a2bb047b9d1112215969c8d924
Enzo Innocenzi <enzo@innocenzi.dev>
2020-06-23 05:03:50 +0200

    feat: add message after installation

--- preset.js
+++ preset.js
@@ -1,11 +1,16 @@
-const { Preset } = require('use-preset');
+const { Preset, Logger } = require('use-preset');
 
 module.exports = Preset.make({
-	name: 'Tailwind CSS preset',
+	name: 'Tailwind CSS with Vite',
 	actions: () => [
 		{
 			type: 'copy',
 			files: '**/**',
 		},
 	],
+	after: ({ generator }) => {
+		Logger.info(
+			`Use 'yarn' or 'npm install' to install dependencies, then run 'yarn dev' to start Vite.`,
+		);
+	},
 });
